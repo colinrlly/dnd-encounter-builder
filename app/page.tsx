3fd76ee8a756c0b7e23849e8f8babf44daeb47df
Colin Reilly <colinrlly@gmail.com>
2023-10-12 22:33:21 -0600

    Wrote some quick Tailwind to make sure it works

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/firebase/config";
 import { collection, getDocs, query } from "firebase/firestore";
+import Link from 'next/link';
 
 async function getEncounters() {
   const encountersRef = collection(db, "encounters");
@@ -15,11 +16,15 @@ export default async function Home() {
   return (
     <main>
       <h1>Encounters</h1>
-      {encounters.docs.map((doc) => (
-        <div key={doc.id}>
-          <p>{doc.data().name}</p>
-        </div>
-      ))}
+      <ul role="list" className="divide-y flex flex-col space-y-4">
+        {encounters.docs.map((doc) => (
+          <Link href={`/encounters/${doc.id}`}>
+            <li key={doc.id} className="flex justify-between gap-x-6 bg-slate-300 rounded-lg p-4">
+              <p className="text-sm font-semibold leading-6 text-gray-900 m-4">{doc.data().name}</p>
+            </li>
+          </Link>
+        ))}
+      </ul>
     </main>
   )
 }
