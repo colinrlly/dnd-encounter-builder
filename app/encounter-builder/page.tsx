249cc7d93795dc00fc663038a3b5a7093431988a
Colin Reilly <colinrlly@gmail.com>
2023-10-14 16:10:11 -0600

    Format/style encounter builder page and add ability to remove monsters

--- app/encounter-builder/page.tsx
+++ app/encounter-builder/page.tsx
@@ -15,27 +15,41 @@ export default function EncounterBuilder() {
   }, []);
 
   return (
-    <main>
+    <div className="prose max-w-none">
       <h1>Monsters</h1>
-      <div>
-        <ul>
-          {availableMonsters.map((monster: Monster) => (
-            <li
-              key={monster.slug}
-              onClick={() => setMonsters([...monsters, monster])}>
 
-              <button>{monster.name}</button>
-            </li>
+      <label className="label">
+        <h2 className="label-text">Encounter Name</h2>
+      </label>
+      <input type="text" placeholder="epic tpk encounter" className="input input-bordered" />
+
+      <div className="flex w-full gap-10">
+        <div className="basis-1/2">
+          <h2>Available Monsters</h2>
+          {availableMonsters.map((monster: Monster) => (
+            <button
+              className="btn m-1"
+              onClick={() => setMonsters([...monsters, monster])}
+              key={monster.slug}
+            >
+              {monster.name}
+            </button>
           ))}
-        </ul>
-        <ul>
+        </div>
+
+        <div className="basis-1/2">
+          <h2>Selected Monsters</h2>
           {monsters.map((monster: Monster) => (
-            <li key={monster.slug}>
-              <p>{monster.name}</p>
-            </li>
+            <button
+              className="btn m-1"
+              onClick={() => setMonsters(monsters.filter(m => m.slug !== monster.slug))}
+              key={monster.slug}
+            >
+              {monster.name}
+            </button>
           ))}
-        </ul>
+        </div>
       </div>
-    </main>
+    </div>
   )
 }
