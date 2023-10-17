77bc2a11dd8104d596be8a1b73e24fe1f77fc7e3
Colin Reilly <colinrlly@gmail.com>
2023-10-17 09:42:27 -0600

    Added github link

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ToastProvider } from "@/components";
 import Link from "next/link";
+import GithubMark from "@/public/github-mark.svg";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,10 +21,17 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="retro">
       <body className={`${inter.className}`}>
-        <div className="navbar bg-secondary">
-          <Link href="/">
-            <p className="btn btn-ghost normal-case text-xl text-white">home</p>
+        <div className="navbar bg-base-300 flex justify-between">
+          <Link href="/" className="block">
+            <p className="btn btn-ghost normal-case text-xl">home</p>
           </Link>
+          <div>
+            <Link href="https://github.com/colinrlly/dnd-encounter-builder">
+              <button className="btn btn-square btn-ghost">
+                <Image src={GithubMark} alt="Github" className="w-10 h-10" />
+              </button>
+            </Link>
+          </div>
         </div>
         <div className="p-8">
           <ToastProvider>{children}</ToastProvider>
