c4cc4c49f7208b67efd8ebbd6e8ddb8b20ecafe0
Colin Reilly <colinrlly@gmail.com>
2023-10-17 10:01:19 -0600

    Fixed open5e api bug

--- api/hydrateSelectedMonsters.ts
+++ api/hydrateSelectedMonsters.ts
@@ -7,9 +7,11 @@ export default async function hydrateSelectedMonsters(
 ): Promise<SelectedMonster[]> {
   const slugs = selectedMonsters
     .map((m: UnhydratedSelectedMonster) => m.slug)
-    .join(",");
+    .join("%2C");
 
-  const res = await fetch(`${OPEN5E_API_URL}/monsters/?slug_in=${slugs}`);
+  const open5eUrl = `${OPEN5E_API_URL}/monsters/?slug__in=${slugs}&slug__iexact=&slug=&name__iexact=&name=&desc__iexact=&desc=&desc__in=&desc__icontains=&cr=&cr__range=&cr__gt=&cr__gte=&cr__lt=&cr__lte=&armor_class=&armor_class__range=&armor_class__gt=&armor_class__gte=&armor_class__lt=&armor_class__lte=&type__iexact=&type=&type__in=&type__icontains=&page_no=&page_no__range=&page_no__gt=&page_no__gte=&page_no__lt=&page_no__lte=&document__slug__iexact=&document__slug=&document__slug__in=&document__slug__not_in=`;
+
+  const res = await fetch(open5eUrl);
   const monsters = await res.json();
 
   const hydratedSelectedMonsters = selectedMonsters.map(
